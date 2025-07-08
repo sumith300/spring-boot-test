eb9963afc999935a9dc69c328a80f2c2cba649fe
sumith300 <sumithmeena987@gmail.com>
2025-07-08 16:16:52 +0530

    added controller

--- src/main/java/tech/sumithmeena/springboottest/hello.java
+++ src/main/java/tech/sumithmeena/springboottest/hello.java
@@ -1,4 +1,14 @@
 package tech.sumithmeena.springboottest;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class hello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
